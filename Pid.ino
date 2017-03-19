c5b4e7ccd48ed835b3ee9ade34b62130f7e772ac
John Carrino <carrino@palantir.com>
2017-03-18 22:11:33 -0700

    improve pid

--- Pid.ino
+++ Pid.ino
@@ -1,32 +1,34 @@
-#include "Arduino.h"
+#include <Arduino.h>
 
 #define MOVING_AVERAGE_INTERVALS 20
-#define PID_INTERVAL 100
+#define PID_INTERVAL_MS 100
+#define PID_INTERVAL_S (PID_INTERVAL_MS / 1000.0)
+#define PID_INTERVAL_HZ (1000.0 / PID_INTERVAL_MS)
 #define THERM_INTERVAL 10
-#define THERM_PIN A1
-#define RELAY_PIN 1
-#define WINDOW_SIZE 1009 // this is prime which gets us nice stuff
-
-float target = 37.0; // body temp
-
-float kp;  // This has units of output/input
-float ki;  // This has units of output/input/s
-float kd;  //This has units of output/(input/s)
+#define THERM_PIN 1
+#define RELAY_PIN 0
+#define WINDOW_SIZE 3001 // this is prime which gets us nice stuff
+#define MIN_WATTS 0.0
+#define MAX_WATTS 300.0
+#define KP 1.0 // This has units of watts/C.  Watts per degree error in C
+#define KI 1.0 // This has units of watts/C/s
+#define KD 1.0 //This has units of watts/(C/s)
+#define TARGET_TEMP 37.0 // body temp
 
 // This is the internal state of our PID
 float lastTemp;
-float accumulatedI = 0;
+float lastError = 0.0;
+float accumulatedI = 0.0;
 
 // Run a simple moving avg on temp to remove noise
 int lastTempUpdate;
 int lastPidUpdate;
 int relayWindowStart;
 float movingAvgTemp;
-
-int output = 0; // between [0, window size]
+int relayOut = 0; // between [0, window size]
 
 void setup() { 
-  Serial.begin(115200);
+  //Serial.begin(115200);
   pinMode(RELAY_PIN, OUTPUT);
   pinMode(THERM_PIN, INPUT);
 
@@ -38,52 +40,62 @@ void setup() {
 } 
 
 void loop(void) {
-  int millis = millis();
+  int now = millis();
   float temp = getTemp();
-  if (millis - lastTempUpdate > THERM_INTERVAL) {
-    lastTempUpdate = millis;
+  if (now - lastTempUpdate > THERM_INTERVAL) {
+    lastTempUpdate = now;
     movingAvgTemp = (movingAvgTemp * (MOVING_AVERAGE_INTERVALS - 1) + temp) / MOVING_AVERAGE_INTERVALS;
   }
 
-  if (millis - lastPidUpdate > PID_INTERVAL) {
-    lastPidUpdate = millis;
-    output = updatePid(movingAvgTemp); 
+  if (now - lastPidUpdate > PID_INTERVAL_MS) {
+    lastPidUpdate = now;
+    // TODO: consider turning off the relay during the computation if it takes too long
+    relayOut = updatePid(movingAvgTemp) * WINDOW_SIZE / MAX_WATTS; 
   }
 
-  if (millis - relayWindowStart >= WINDOW_SIZE) {
-    relayWindowStart = millis;
+  if (now - relayWindowStart >= WINDOW_SIZE) {
+    relayWindowStart += WINDOW_SIZE;
   }
 
-  if (millis - relayWindowStart < output) {
-    digitalWire(RELAY_PIN, HIGH);
+  if (now - relayWindowStart < relayOut) {
+    digitalWrite(RELAY_PIN, HIGH);
   } else {
-    digitalWire(RELAY_PIN, LOW);
+    digitalWrite(RELAY_PIN, LOW);
   }
 
 }
 
-int updatePid(float temp) {
-  float error = target - temp;
-  float delta = lastTemp - temp;
+// The output of the method is watts between 0 and 300W
+float updatePid(float temp) {
+  float error = TARGET_TEMP - temp; // unit of deg C
+  float delta = lastTemp - temp; // unit of deg C
+
+  float p = KP * error; // unit of W
+  float normP = norm(p); // unit of W
+
+  // We average lastError and error to use the trapezoidal approximation of the integral instead of the box one.
+  accumulatedI += KI * (error + lastError) * 0.5 * PID_INTERVAL_S;
+  float d = KD * delta * PID_INTERVAL_HZ; // unit of W
+
+  // We don't want the accumulated integral term to be higher than what is needed to get up to the max output.
+  // We don't need the integral to add more value if we are already at full blast.
+  // This will reduce integral windup https://en.wikipedia.org/wiki/Integral_windup
+  // This method is better at reducing windup than direct clamping at min and max.
+  accumulatedI = min(accumulatedI, MAX_WATTS - normP);
+  accumulatedI = max(accumulatedI, MIN_WATTS - normP);
+
   lastTemp = temp;
-
-  if (error < 0) {
-    accumulatedI = 0;
-    return 0;
-  }
-
-  float p = kp * error;
-  float accumulatedI += ki * error; // the time of 100ms is implicit in the tuning param ki
-  float d = kd * delta;
-
-  // We don't want the accumulated integral term to be higher than what it needed to get up to the max output.
-  accumulatedI = min(accumulatedI, WINDOW_SIZE - p);
-  accumulatedI = max(accumulatedI, 0);
-  return min(WINDOW_SIZE, p + accumulatedI + d);
+  lastError = error;
+  return norm(p + accumulatedI + d);
 }
+
+float norm(float f) {
+  return max(MIN_WATTS, min(MAX_WATTS, f));
+}
+
 
 float getTemp() {
   int val = analogRead(THERM_PIN);
   // convert to temp
-  return temp;
+  return 37;
 }

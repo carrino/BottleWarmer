2a4d3ef55b5537c1591aabcb5bb27ae7b9c13adb
John Carrino <carrino@palantir.com>
2017-03-19 16:17:57 -0700

    add led out

--- Pid.ino
+++ Pid.ino
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 
-#define MOVING_AVERAGE_INTERVALS 20
+#define MOVING_AVERAGE_INTERVALS 32 // This should be a power of 2 for perf
 #define PID_INTERVAL_MS 100
 #define PID_INTERVAL_S (PID_INTERVAL_MS / 1000.0)
 #define PID_INTERVAL_HZ (1000.0 / PID_INTERVAL_MS)
@@ -8,6 +8,7 @@
 #define THERM_PIN 1
 #define RELAY_PIN 0
 #define BUTTON_PIN 3
+#define LED_PIN 4
 #define WINDOW_SIZE 2999 // this is prime which gets us nice stuff
 #define MIN_WATTS 0.0
 #define MAX_WATTS 300.0
@@ -23,38 +24,42 @@ float lastError = 0.0;
 float accumulatedI = 0.0;
 
 bool waitForButton = true;
-int lastButtonPress;
+long lastButtonPress;
 int lastTempUpdate;
 int lastPidUpdate;
 int relayWindowStart;
-float movingAvgTemp; // Run a simple moving avg on temp to remove noise
-int relayOut = 0; // between [0, window size]
+float movingAvgTemp; // Run a simple exponential moving avg on temp to remove noise
+int relayWindowOut = 0; // between [0, window size]
 
 void setup() { 
   //Serial.begin(115200);
   pinMode(RELAY_PIN, OUTPUT);
-  pinMode(THERM_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
 
-  lastPidUpdate = millis();
-  lastTempUpdate = lastPidUpdate;
-  relayWindowStart = lastPidUpdate;
-  lastButtonPress = lastPidUpdate;
+  lastButtonPress = millis();
+  lastPidUpdate = lastButtonPress;
+  lastTempUpdate = lastButtonPress;
+  relayWindowStart = lastButtonPress;
+  lastButtonPress = lastButtonPress;
   lastTemp = getTemp();
   movingAvgTemp = lastTemp;
 } 
 
 void loop(void) {
-  int now = millis();
+  long now = millis();
   float temp = getTemp();
   if (now - lastTempUpdate > THERM_INTERVAL) {
     lastTempUpdate = now;
+    // This is a modified moving average (MMA): https://en.wikipedia.org/wiki/Moving_average#Modified_moving_average
+    // basically a exponential moving average with alpha set to 1/MOVING_AVERAGE_INTERVALS
     movingAvgTemp = (movingAvgTemp * (MOVING_AVERAGE_INTERVALS - 1) + temp) / MOVING_AVERAGE_INTERVALS;
   }
 
-  int buttonDown = digitalRead(BUTTON_PIN);
-  if (buttonDown == HIGH) {
+  if (digitalRead(BUTTON_PIN) == LOW) {
     lastButtonPress = now;
     waitForButton = false;
+    digitalWrite(LED_PIN, HIGH);
   }
 
   if (now - lastButtonPress > RUN_TIME) {
@@ -63,26 +68,31 @@ void loop(void) {
 
   if (waitForButton) {
     digitalWrite(RELAY_PIN, LOW);
+    digitalWrite(LED_PIN, LOW);
     return;
-  }
-
-  if (now - lastPidUpdate > PID_INTERVAL_MS) {
-    lastPidUpdate = now;
-    // TODO: consider turning off the relay during the computation if it takes too long
-    relayOut = updatePid(movingAvgTemp) * 10; 
   }
 
   if (now - relayWindowStart >= WINDOW_SIZE) {
     relayWindowStart += WINDOW_SIZE;
   }
 
-  if (now - relayWindowStart < relayOut) {
+  if (now - lastPidUpdate > PID_INTERVAL_MS) {
+    lastPidUpdate = now;
+    // TODO: consider turning off the relay during the computation if it takes too long
+    relayWindowOut = updatePid(movingAvgTemp) * 10; 
+  }
+
+  updateRelay(now - relayWindowStart);
+}
+
+void updateRelay(int windowMillis) {
+  if (windowMillis < relayWindowOut) {
     digitalWrite(RELAY_PIN, HIGH);
   } else {
     digitalWrite(RELAY_PIN, LOW);
   }
-
 }
+
 
 // The output of the method is watts between 0 and 300W
 float updatePid(float temp) {

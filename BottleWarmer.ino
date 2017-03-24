63b27e69bc180b5dcc7416533423df186efcd7fc
John Carrino <carrino@palantir.com>
2017-03-23 17:00:44 -0700

    backward relay

--- BottleWarmer.ino
+++ BottleWarmer.ino
@@ -4,8 +4,8 @@
 #if defined(ARDUINO_AVR_TRINKET3) || defined(ARDUINO_AVR_TRINKET5)
 #define THERM_PIN 1 // this is analog 1 which is digial 2
 #define RELAY_PIN 0
-#define BUTTON_PIN 3
-#define LED_PIN 1
+#define BUTTON_PIN 3 // This is a shared usb pin, but our button pulls to ground so it should work
+#define LED_PIN 1 // LED pin required PWM
 #else
 #define THERM_PIN A1
 #define RELAY_PIN 9
@@ -22,34 +22,30 @@
 #define WINDOW_SIZE 2999 // this is prime which gets us nice stuff
 #define TARGET_TEMP 37.0 // body temp
 #define RUN_TIME (15L*60*1000) // 15 min
+#define LED_PWM_OUT 128
 
 // Define Hardware contstants
+#define RELAY_ON LOW  // our relay is backwards it seems
+#define RELAY_OFF HIGH
 #define MIN_WATTS 0.0
 #define MAX_WATTS 300.0
 #define KNOWN_RESISTOR 10000.0
 #define THERMISTOR_BETA 3950.0
 #define THERMISTOR_ROOM_TEMP_K 298.15
 #define THERMISTOR_ROOM_RESIST 10000.0
+
 // In testing we found that we can go full blast to about 10 degrees out from the target.
 // We want to avoid overshoot.
 // This means that KP should be about 1/10 of max watts to start backing off at 10degC out.
-#define KP (MAX_WATTS/10.0)  // This has units of watts/C.  Watts per degree error in C
-
-// KI should be very small.  At steady state, we lost less than 1 degree C over 7 min of testing.
-// This is 1kJ over 7min or about 2.3 watts.  This means our steady state is only 2.3W/KP which is about .1 degC
-// Our thermometer isn't even this accurate, so KI should be 0
-#define KI 0.0 // This has units of watts/C/s
+#define KP (MAX_WATTS / 10.0)  // This has units of watts/C.  Watts per degree error in C
 
 // In testing we found that a rate of .3 degrees C per second was full blast with about 240cc of water
 // We also found that a cutoff at full blast may still rise 5 degrees.
 // To be on the safe side we want to full cut off heat at 5 degrees out when rising at 3 sec per C
 // At 5C error we will be running at half power so we should cut 150W per .3C/s
-#define KD (KP / 5.0 * 3.0) //This has units of watts/(C/s) or W * (s/C)
+#define KD (KP * 5.0 * 3.0) //This has units of watts/(C/s) or W * (s/C)
 
-// This is the internal state of our PID like code
 float lastTemp;
-//float accumulatedI = 0.0;
-
 bool waitForButton = true;
 long lastButtonPress;
 int lastTempUpdate;
@@ -62,6 +58,8 @@ void setup() {
   //Serial.begin(115200);
 
   pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, RELAY_OFF);
+
   pinMode(LED_PIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
 
@@ -89,7 +87,7 @@ void loop(void) {
   if (digitalRead(BUTTON_PIN) == LOW) {
     lastButtonPress = now;
     waitForButton = false;
-    digitalWrite(LED_PIN, HIGH);
+    analogWrite(LED_PIN, LED_PWM_OUT);
   }
 
   if (now - lastButtonPress > RUN_TIME) {
@@ -97,7 +95,7 @@ void loop(void) {
   }
 
   if (waitForButton) {
-    digitalWrite(RELAY_PIN, LOW);
+    digitalWrite(RELAY_PIN, RELAY_OFF);
     digitalWrite(LED_PIN, LOW);
     return;
   }
@@ -113,29 +111,22 @@ void loop(void) {
     //Serial.print("temp: "); Serial.println(movingAvgTemp);
   }
 
-  updateRelay(now - relayWindowStart);
-  //Serial.print("loop time "); Serial.println(millis() - now);
-}
-
-void updateRelay(int windowMillis) {
-  if (windowMillis < relayWindowOut) {
-    digitalWrite(RELAY_PIN, HIGH);
+  if (now - relayWindowStart < relayWindowOut) {
+    digitalWrite(RELAY_PIN, RELAY_ON);
   } else {
-    digitalWrite(RELAY_PIN, LOW);
+    digitalWrite(RELAY_PIN, RELAY_OFF);
   }
+  //Serial.print("loop time "); Serial.println(millis() - now);
 }
 
 // The output of the method is watts between min and max
 float updatePid(float temp) {
   float error = TARGET_TEMP - temp; // unit of deg C
-  float lastError = TARGET_TEMP - lastTemp; // unit of deg C
   float delta = lastTemp - temp; // unit of deg C
 
   float p = KP * error; // unit of W
   float normP = norm(p); // unit of W
 
-  // We average lastError and error to use the trapezoidal approximation of the integral instead of the box one.
-  //accumulatedI += KI * (error + lastError) * 0.5 * PID_INTERVAL_S;
   float d = KD * delta * PID_INTERVAL_HZ; // unit of W
   if (d > 0) {
     // We only want our d term to slow us down when heating.  
@@ -143,15 +134,7 @@ float updatePid(float temp) {
     d = 0;
   }
 
-  // We don't want the accumulated integral term to be higher than what is needed to get up to the max output.
-  // We don't need the integral to add more value if we are already at full blast.
-  // This will reduce integral windup https://en.wikipedia.org/wiki/Integral_windup
-  // This method is better at reducing windup than direct clamping at min and max.
-  //accumulatedI = min(accumulatedI, MAX_WATTS - normP);
-  //accumulatedI = max(accumulatedI, MIN_WATTS - normP);
-
   lastTemp = temp;
-  //return norm(p + accumulatedI + d);
   return norm(p + d);
 }
 
